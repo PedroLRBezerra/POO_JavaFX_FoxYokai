8d135409ab36ff911dfce9ae8ac9e6ca7655812e
RajGod <pedro.luiz@hotmail.com.br>
2019-10-18 23:35:51 -0300

    re-org

--- src/TelaPrincipal.java
+++ src/TelaPrincipal.java
@@ -12,6 +12,31 @@ import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class TelaPrincipal extends Application {
+	
+	 double protagonistaX=250;
+	 double protagonistaY=330;
+	 
+	 double protagonistaYLimite=330;
+	 
+	 double velocidade=5;
+	 double boost =1.0;
+	 
+	 int isJumping = 1;
+
+	 
+	 Image imgFloresta = new Image(getClass().getResourceAsStream("/images/floresta1.png"));
+	 
+	 Image imgProtagonista = new Image(getClass().getResourceAsStream("/images/protagonista.png"));
+	
+	 Image imgHatsune = new Image(getClass().getResourceAsStream("/images/hatsuneinimigo.png"));
+		
+	 double hatsuX=metadeImagemX(imgHatsune, protagonistaX + 400);
+	 double hatsuY=metadeImagemY(imgHatsune, protagonistaY);
+	 
+	 Canvas canvas = new Canvas(imgFloresta.getWidth(), imgFloresta.getHeight());
+	 
+	 GraphicsContext ctx = canvas.getGraphicsContext2D();
+	
 	public double metadeImagemX(Image img, double x) { 
 		return x - img.getWidth() / 2;
 	}
@@ -22,58 +47,41 @@ public class TelaPrincipal extends Application {
 	
 
 	
-	 double x=380;
-	 double y=300;
-	 
-	 double velocidade=5;
-	 double boost =1.0;
-	 
-	 int isJumping = 1;
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Image imgFloresta = new Image(
-				getClass().getResourceAsStream("/images/floresta1.png"));
-		Image imgProtagonista = new Image(
-				getClass().getResourceAsStream("/images/protagonista.png"));
-		Image imgHatsune = new Image(
-				getClass().getResourceAsStream("/images/hatsuneinimigo.png"));
+		
 
 		double centerX = imgFloresta.getWidth() / 2;
 		double centerY = imgFloresta.getHeight() / 2;
+		
+		
 		Group grp = new Group();
 		Scene scn = new Scene(grp, imgFloresta.getWidth(), imgFloresta.getHeight());
 		
-		Canvas canvas = new Canvas(imgFloresta.getWidth(), imgFloresta.getHeight());
+		
 		grp.getChildren().add(canvas);
 		
-		GraphicsContext ctx = canvas.getGraphicsContext2D();
 		
 		EventHandler<KeyEvent> manipulador = new Manipulador();
 		EventHandler<KeyEvent> manipuladorRelease = new ManipuladorReleased();
 		stage.addEventFilter(KeyEvent.KEY_PRESSED,manipulador);
 		stage.addEventFilter(KeyEvent.KEY_RELEASED, manipuladorRelease);
 		
+		paint();
+		
 		
 		new AnimationTimer() {
 			
 			@Override
 			public void handle(long now) {
-
-				double centerflorestaX = metadeImagemX(imgProtagonista, centerX);
-				double centerflorestaY = metadeImagemY(imgProtagonista, centerY);
-				
-				double centerhatX = metadeImagemX(imgHatsune, centerX + 400);
-				double centerhatY = metadeImagemY(imgHatsune, centerY);
-				
-				
-				ctx.drawImage(imgFloresta, 0, 0);
-				ctx.drawImage(imgProtagonista, centerflorestaX, centerflorestaY);
-				ctx.drawImage(imgHatsune, centerhatX, centerhatY);
-				move(0,velocidade*isJumping*boost);
+				//protagonistaMover(0,velocidade*isJumping*boost);
 				if(boost>1) {
 					boost-=0.015;
 				}
+				
+				
+				paint();
 			}
 		}.start();
 		
@@ -83,13 +91,19 @@ public class TelaPrincipal extends Application {
 	}
 	
 	
+	private void paint() {
+		ctx.drawImage(imgFloresta, 0, 0);
+		ctx.drawImage(imgProtagonista, protagonistaX, protagonistaY);
+		ctx.drawImage(imgHatsune, hatsuX, hatsuY);
+	}
+	
 	class Manipulador implements EventHandler<KeyEvent> {
 
 		@Override
 		public void handle(KeyEvent event) {
 			if (event.getCode() == KeyCode.UP) {
-		    	if(y<=0 ||!(x>0 && x< 600) ) {
-		    		move(0,0);
+		    	if(protagonistaY<=0 ||!(protagonistaX>0 && protagonistaX< 600) ) {
+		    		protagonistaMover(0,0);
 				} else {
 					isJumping=-1;
 					boost=2;
@@ -103,14 +117,14 @@ public class TelaPrincipal extends Application {
 
 		@Override
 		public void handle(KeyEvent event) {
-			move(0,0);
+			protagonistaMover(0,0);
 		}
 		
 	}
 	
-	public void move(double x, double y) {
-		this.x +=x;
-		this.y +=y;
+	public void protagonistaMover(double x, double y) {
+		this.protagonistaX +=x;
+		this.protagonistaY +=y;
 	}
 	
 	
